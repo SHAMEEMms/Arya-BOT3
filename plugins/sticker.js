84acba02918d53f61f6fc08afdf81d487d0f57d9
SHAMEEMms <79793462+SHAMEEMms@users.noreply.github.com>
2021-03-01 08:29:10 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,41 +1,32 @@
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
-const {execFile} = require('child_process');
-const cwebp = require('cwebp-bin');
+const { MessageType } = require('@adiwajshing/baileys')
+const { sticker, sticker2 } = require('../lib/sticker')
+let handler  = async (m, { conn, args }) => {
+  let stiker = false
+  try {
+    let q = m.quoted ? m.quoted : m
+    if (/image/.test(q.mimetype || '')) {
+      let img = await q.download()
+      if (!img) throw img
+      stiker = await sticker2(img)
+    } else if (args[0]) stiker = await sticker2(false, args[0])
+  } finally {
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+  }
+}
+handler.help = ['stiker (caption|reply media)', 'stiker <url>']
+handler.tags = ['sticker']
+handler.command = /^stic?ker$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-const Language = require('../language');
-const Lang = Language.getString('sticker');
+handler.admin = false
+handler.botAdmin = false
 
-Asena.addCommand({pattern: 'sticker', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
+handler.fail = null
 
-    if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
-    var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
-    var location = await message.client.downloadAndSaveMediaMessage({
-        key: {
-            remoteJid: message.reply_message.jid,
-            id: message.reply_message.id
-        },
-        message: message.reply_message.data.quotedMessage
-    });
-
-    if (message.reply_message.video === false && message.reply_message.image) {
-        execFile(cwebp, [location, '-o', 'output.webp'], async err => {
-            if (err) {
-                throw err;
-            }
-        
-            await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
-        });
-        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-    }
-
-    ffmpeg(location)
-        .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 512x512"])
-        .save('sticker.webp')
-        .on('end', async () => {
-            await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
-        });
-    return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-}));
+module.exports = handler
